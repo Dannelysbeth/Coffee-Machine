52619fb9121f56674afea2f93abc19ef152b9f3c
danifoj315 <danifoj315@student.polsl.pl>
2023-04-15 23:29:39 +0200

    added first part of project

---
+++ src/main/java/org/hiperskill/Main.java
@@ -0,0 +1,15 @@
+package org.hiperskill;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+            Starting to make a coffee
+            Grinding coffee beans
+            Boiling water
+            Mixing boiled water with crushed coffee beans
+            Pouring coffee into the cup
+            Pouring some milk into the cup
+            Coffee is ready!""");
+    }
+    }
+}
