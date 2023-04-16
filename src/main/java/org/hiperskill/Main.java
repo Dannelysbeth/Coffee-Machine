7e997b256744c25581aec0510598e1f0d132b17f
danifoj315 <danifoj315@student.polsl.pl>
2023-04-16 23:54:50 +0200

    added coffee recipe

--- src/main/java/org/hiperskill/Main.java
+++ src/main/java/org/hiperskill/Main.java
@@ -1,15 +1,16 @@
 package org.hiperskill;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
-            Starting to make a coffee
-            Grinding coffee beans
-            Boiling water
-            Mixing boiled water with crushed coffee beans
-            Pouring coffee into the cup
-            Pouring some milk into the cup
-            Coffee is ready!""");
-    }
+        System.out.println("Write how many cups of coffee you will need: ");
+        Scanner scanner = new Scanner(System.in);
+        int amount = scanner.nextInt();
+        System.out.printf("""
+            For %d cups of coffee you will need:
+            %d ml of water
+            %d ml of milk
+            %d g of coffee beans""", amount, amount*200, amount*50, amount*15);
     }
 }
