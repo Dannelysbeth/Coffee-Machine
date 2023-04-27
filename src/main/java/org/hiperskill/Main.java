ef48f7016aa0005c038bd6d488097fb46502b858
danifoj315 <danifoj315@student.polsl.pl>
2023-04-27 23:50:00 +0200

    stage of 5 completed

--- src/main/java/org/hiperskill/Main.java
+++ src/main/java/org/hiperskill/Main.java
@@ -3,83 +3,67 @@ package org.hiperskill;
 import java.util.Scanner;
 
 public class Main {
-    private static final int WATER_PER_CUP = 200; // Milliliters
-    private static final int MILK_PER_CUP = 50; // Milliliters
-    private static final int COFFEE_BEANS_PER_CUP = 15; // Grams
-
     private static int waterAmount = 400;
     private static int milkAmount = 540;
     private static int beansAmount = 120;
     private static int cupsAmount = 9;
     private static int money = 550;
 
+    private static final int ESPRESSO_WATER_REQ = 250;
+    private static final int ESPRESSO_MILK_REQ = 0;
+    private static final int ESPRESSO_BEANS_REQ = 16;
+    private static final int ESPRESSO_PRICE = 4;
+
+    private static final int LATTE_WATER_REQ = 350;
+    private static final int LATTE_MILK_REQ = 75;
+    private static final int LATTE_BEANS_REQ = 20;
+    private static final int LATTE_PRICE = 7;
+
+
+    private static final int CAPPUCCINO_WATER_REQ = 200;
+    private static final int CAPPUCCINO_MILK_REQ = 100;
+    private static final int CAPPUCCINO_BEANS_REQ = 12;
+    private static final int CAPPUCCINO_PRICE = 6;
+
 
     public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
         Scanner scanner = new Scanner(System.in);
-
-//        // Get the amount of water, milk, coffee beans, and cups of coffee from the user
-//        System.out.println("Write how many ml of water the coffee machine has:");
-//        int waterAmount = scanner.nextInt();
-//        System.out.println("Write how many ml of milk the coffee machine has:");
-//        int milkAmount = scanner.nextInt();
-//        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-//        int coffeeBeansAmount = scanner.nextInt();
-//        System.out.println("Write how many cups of coffee you will need:");
-//        int requestedCoffeeCups = scanner.nextInt();
-
-        System.out.printf("""
-                        The coffee machine has:
-                        %d ml of water
-                        %d ml of milk
-                        %d g of coffee beans
-                        %d disposable cups
-                        $%d of money
-                                        
-                        Write action (buy, fill, take):\s
-                        """,
-                waterAmount, milkAmount, beansAmount, cupsAmount, money);
-
+        System.out.println("\nWrite action (buy, fill, take, remaining, exit): ");
         String operation = scanner.next();
         switch (operation) {
             case "buy" -> buy(scanner);
             case "fill" -> fill(scanner);
             case "take" -> take(scanner);
+            case "remaining" -> remaining();
+            case "exit" -> {
+            }
             default -> {
+                System.out.println("Wrong operation!");
+                menu();
             }
         }
+    }
+
+    public static void remaining() {
         System.out.printf("""
+                                                
                         The coffee machine has:
                         %d ml of water
                         %d ml of milk
                         %d g of coffee beans
                         %d disposable cups
                         $%d of money
-                                        
-                        Write action (buy, fill, take):\s
                         """,
                 waterAmount, milkAmount, beansAmount, cupsAmount, money);
-
-
-//        // Calculate the maximum number of cups of coffee that can be made with the given resources
-//        int maxPossibleCups = calculatePossibleCups(waterAmount, milkAmount, coffeeBeansAmount);
-//
-//        // Print the result
-//        if (maxPossibleCups > requestedCoffeeCups) {
-//            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", maxPossibleCups - requestedCoffeeCups);
-//        } else if (maxPossibleCups == requestedCoffeeCups) {
-//            System.out.println("Yes, I can make that amount of coffee");
-//        } else {
-//            System.out.printf("No, I can make only %d cup(s) of coffee", maxPossibleCups);
-//        }
-    }
-
-    public static int calculatePossibleCups(int waterAmount, int milkAmount, int coffeeBeansAmount) {
-        // Calculate the maximum number of cups of coffee that can be made with the given resources
-        return Math.min(Math.min(waterAmount / WATER_PER_CUP, milkAmount / MILK_PER_CUP), coffeeBeansAmount / COFFEE_BEANS_PER_CUP);
+        menu();
     }
 
     public static void fill(Scanner scanner) {
-        System.out.println("Write how many ml of water you want to add: ");
+        System.out.println("\nWrite how many ml of water you want to add: ");
         waterAmount = waterAmount + scanner.nextInt();
         System.out.println("Write how many ml of milk you want to add: ");
         milkAmount = milkAmount + scanner.nextInt();
@@ -87,55 +71,71 @@ public class Main {
         beansAmount = beansAmount + scanner.nextInt();
         System.out.println("Write how many disposable cups you want to add: ");
         cupsAmount = cupsAmount + scanner.nextInt();
+        menu();
     }
 
     public static void take(Scanner scanner) {
-        System.out.println("Write how many ml of water you want to add: ");
-        System.out.printf("I gave you $%d", money);
+        System.out.printf("\nI gave you $%d \n", money);
         money = 0;
+        menu();
     }
 
     public static void buy(Scanner scanner) {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        int coffeeType = scanner.nextInt();
+        System.out.println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
+        String coffeeType = scanner.next();
+        StringBuilder msg = new StringBuilder();
         switch (coffeeType) {
-            case 1 -> {                 //espresso For one espresso, the coffee machine needs 250 ml of water and 16 g of coffee beans. It costs $4.
-                if (checkIfCanMakeEspresso()) {
-                    waterAmount -= 250;
-                    beansAmount -= 16;
-                    money += 4;
-                } else {
-                    System.out.println("");
+            case "1" -> {                 //espresso For one espresso, the coffee machine needs 250 ml of water and 16 g of coffee beans. It costs $4.
+                msg = new StringBuilder(getIfCanMakeCoffeeMsg(ESPRESSO_WATER_REQ, ESPRESSO_MILK_REQ, ESPRESSO_BEANS_REQ));
+                if (msg.toString().equals("I have enough resources, making you a coffee!")) {
+                    waterAmount -= ESPRESSO_WATER_REQ;
+                    beansAmount -= ESPRESSO_BEANS_REQ;
+                    money += ESPRESSO_PRICE;
+                    cupsAmount--;
                 }
-
             }
-            case 2 -> {                 //For a latte, the coffee machine needs 350 ml of water, 75 ml of milk, and 20 g of coffee beans. It costs $7.
-                waterAmount -= 350;
-                milkAmount -= 75;
-                beansAmount -= 20;
-                money += 7;
+            case "2" -> {                 //For a latte, the coffee machine needs 350 ml of water, 75 ml of milk, and 20 g of coffee beans. It costs $7.
+                msg = new StringBuilder(getIfCanMakeCoffeeMsg(LATTE_WATER_REQ, LATTE_MILK_REQ, LATTE_BEANS_REQ));
+                if (msg.toString().equals("I have enough resources, making you a coffee!")) {
+                    waterAmount -= LATTE_WATER_REQ;
+                    milkAmount -= LATTE_MILK_REQ;
+                    beansAmount -= LATTE_BEANS_REQ;
+                    money += LATTE_PRICE;
+                    cupsAmount--;
+                }
             }
-            case 3 -> {                 //cappuccino, the coffee machine needs 200 ml of water, 100 ml of milk, and 12 g of coffee beans. It costs $6.
-                waterAmount -= 200;
-                milkAmount -= 100;
-                beansAmount -= 12;
-                money += 6;
+            case "3" -> {                 //cappuccino, the coffee machine needs 200 ml of water, 100 ml of milk, and 12 g of coffee beans. It costs $6.
+                msg = new StringBuilder(getIfCanMakeCoffeeMsg(CAPPUCCINO_WATER_REQ, CAPPUCCINO_MILK_REQ, CAPPUCCINO_BEANS_REQ));
+                if (msg.toString().equals("I have enough resources, making you a coffee!")) {
+                    waterAmount -= CAPPUCCINO_WATER_REQ;
+                    milkAmount -= CAPPUCCINO_MILK_REQ;
+                    beansAmount -= CAPPUCCINO_BEANS_REQ;
+                    money += CAPPUCCINO_PRICE;
+                    cupsAmount--;
+                }
+            }
+            case "back" -> {
             }
             default -> {
             }
         }
-        cupsAmount = cupsAmount > 0 ? cupsAmount-- : cupsAmount;
+        System.out.println(msg);
+//        cupsAmount = cupsAmount > 0 ? cupsAmount-- : cupsAmount;
+        menu();
     }
 
     public static boolean checkIfAnyCups() {
         return cupsAmount > 0 ? true : false;
     }
 
-    public static boolean checkIfCanMakeEspresso() {
-        return waterAmount >= 250 ?
-                (beansAmount >= 16 ?
-                        (checkIfAnyCups() ? true : false) : false) : false;
+    public static String getIfCanMakeCoffeeMsg(int waterReq, int milkReq, int beansReq) {
+        return waterAmount >= waterReq ?
+                (milkAmount >= milkReq ?
+                        (beansAmount >= beansReq ?
+                                (checkIfAnyCups() ? "I have enough resources, making you a coffee!" :
+                                        "Sorry, not enough disposable cups!") :
+                                "Sorry, not enough coffee beans!") :
+                        "Sorry, not enough milk!") :
+                "Sorry, not enough water!";
     }
-
-
 }
